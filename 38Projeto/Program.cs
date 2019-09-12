88cc6972fe2ec3e3f5771c6561f86cd633916c46
Isaac Junior <maisaacjr@yahoo.com.br>
2019-09-12 18:25:57 -0300

    Acrescentada a pergunta sobre como voce estah

--- 38Projeto/Program.cs
+++ 38Projeto/Program.cs
@@ -8,6 +8,7 @@ namespace _38Projeto
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Bom dia!");
+            Console.WriteLine("Como você está hoje?");
             Console.WriteLine();
         }
     }
