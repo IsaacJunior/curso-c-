e3a98d8913112cb357f514182540a809015a9501
Isaac Junior <maisaacjr@yahoo.com.br>
2019-09-12 14:00:06 -0300

    Projeto iniciado, mostrando ola mundo

---
+++ 38Projeto/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _38Projeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
