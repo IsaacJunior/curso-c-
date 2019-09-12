8312963fdca6218706e073a04581e056b365e170
Isaac Junior <maisaacjr@yahoo.com.br>
2019-09-12 14:02:17 -0300

    Acrescentada uma mensagem de bom dia

--- 38Projeto/Program.cs
+++ 38Projeto/Program.cs
@@ -7,6 +7,8 @@ namespace _38Projeto
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Bom dia!");
+            Console.WriteLine();
         }
     }
 }
